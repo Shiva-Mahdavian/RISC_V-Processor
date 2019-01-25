cbd076ed822e7355320af0be655ed90b1f0750be
Shiva <mahdavianshiva@gmail.com>
2019-01-25 14:29:53 +0330

    Change to SystemVerilog standard and Add default for case to be combinational

--- ALU.sv
+++ ALU.sv
@@ -3,12 +3,13 @@ module ALU (input  logic signed [63:0] SrcA, SrcB,
 	    output logic 	Zero,
 	    output logic signed [63:0]	ALURes);
 	    
-	always@(ALUControl, SrcA, SrcB) begin
+	always_comb begin
 		case(ALUControl)
-			4'b0000: ALURes <= SrcA & SrcB;
-			4'b0001: ALURes <= SrcA | SrcB;
-			4'b0010: ALURes <= SrcA + SrcB;
-			4'b0110: ALURes <= SrcA - SrcB;
+			4'b0000: ALURes = SrcA & SrcB;
+			4'b0001: ALURes = SrcA | SrcB;
+			4'b0010: ALURes = SrcA + SrcB;
+			4'b0110: ALURes = SrcA - SrcB;
+			default: ALURes = 64'b0;
 		endcase
 	end
 	
